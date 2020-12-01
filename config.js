0c7df5e4f7d767713d70154879de3df90f9741af
Jesus Agustin Peña Meza <jesus@100ladrillos.com>
2020-11-30 23:23:25 -0600

    MK-6 Add the main server file

--- config.js
+++ config.js
@@ -1,13 +1,23 @@
+const RootPath = require('app-root-path');
+const dotenv = require('dotenv');
+
 const knexfile = require('./knexfile');
 
 function buildConfig() {
   const env = process.env.NODE_ENV;
+  const path = `${RootPath}/.env/${env}.env`;
+  const { parsed: envObject } =  dotenv.config({ path });
+
   const databaseData = knexfile[env];
 
   return {
     database: {
       driver: databaseData.client,
       name: databaseData.connection.database
+    },
+    server: {
+      host: envObject.SERVER_HOST,
+      port: Number(envObject.SERVER_PORT)
     }
   };
 }
