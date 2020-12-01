58bf9cb4018092e879fd946b9c25b810a19bf829
Jesús Agustín Peña Meza <jesusx21@gmail.com>
2020-11-30 19:47:41 -0700

    MK-2 Add database driver (#4)

---
+++ knexfile.js
@@ -0,0 +1,64 @@
+// Update with your config settings.
+
+module.exports = {
+  test: {
+    client: 'postgresql',
+    connection: {
+      database: 'loquillo_wallet_test',
+      user:     'postgres'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  development: {
+    client: 'postgresql',
+    connection: {
+      database: 'loquillo_wallet_dev',
+      user:     'postgres'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'loquillo_wallet_staging',
+      user:     'postgres'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'loquillo_wallet',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+};
