88375abcd15601362e5ee74635eed573663fc4e2
Jesús Agustín Peña Meza <jesusx21@gmail.com>
2020-11-30 22:24:24 -0700

    MK-6 Add the main server file (#8)

---
+++ index.js
@@ -0,0 +1,33 @@
+const config = require('./config');
+const buildApp = require('./server/app')
+const buildDatabase = require('./database');
+const drivers = require('./database/drivers');
+const entitiesBuilder = require('./domain/entities');
+
+async function initializeServer() {
+  const db = drivers[config.database.driver];
+  const database = buildDatabase(db, entitiesBuilder);
+
+  const app = buildApp({
+    database,
+    port: config.server.port,
+    host: config.server.host
+  });
+
+  await app.start()
+    .then(() => {
+      console.log(`
+        ||\\\\            //||      ||=======||                                 ||    //        ||=======||            ||===||        ||=======||      ============
+        ||  \\\\        //  ||      ||        ||                                ||  //          ||        ||          ||     ||       ||        ||               //
+        ||    \\\\    //    ||      ||        ||                                ||//            ||        ||         ||       ||      ||        ||             //
+        ||      \\\\//      ||      ||=======||                                 ||\\\\            ||=======||          ||=======||      ||=======||            //
+        ||                ||      ||         \\\\                               ||  \\\\          ||       \\\\          ||       ||      ||        ||         //
+        ||                ||      ||           \\\\     ||==||                  ||    \\\\        ||         \\\\        ||       ||      ||        ||       //
+        ||                ||      ||             \\\\   ||==||                  ||      \\\\      ||           \\\\      ||       ||      ||=======||      ============
+      `);
+
+      console.log('Server runing on %s', app.info.uri);
+    });
+}
+
+initializeServer();
